4c8cefb55d46da5bb88cb4ac7ecf5051a315642f
Raegan Garlitz <raegan.garlitz@gmail.com>
2018-10-20 21:09:07 -0600

    Added films.js and people.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let characterName = document.querySelector('#characterName')
+/*let characterName = document.querySelector('#characterName')
 let hairColor = document.querySelector('#hairColor')
 let eyeColor = document.querySelector('#eyeColor')
 let movieTitle = document.querySelector('#movieTitle')
@@ -7,12 +7,12 @@ fetch('https://swapi.co/api/films', {mode: "no-cors"})
   .then(function(response) {
     return response.json();
   })
-  //.then(function(myJson) {
-    //  characterName.textContent = myJson.name
-      //hairColor.textContent = myJson.hair_color
-      //eyeColor.textContent = myJson.eye_color
-    //console.log(JSON.stringify(myJson));
-  //})
+  .then(function(myJson) {
+    characterName.textContent = myJson.name
+    hairColor.textContent = myJson.hair_color
+    eyeColor.textContent = myJson.eye_color
+    console.log(JSON.stringify(myJson));
+  })
   .then(function(myJson) {
     myJson.results.forEach(function(film) {
       var list = document.createElement('li')
@@ -20,4 +20,8 @@ fetch('https://swapi.co/api/films', {mode: "no-cors"})
       movieTitle.appendChild(list)
       console.log(film.title);
     })
-  })
+  })*/
+  
+
+
+  

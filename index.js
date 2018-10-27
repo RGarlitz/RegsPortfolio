4929d0a9ebe75063264e4b2ebceeea1844d20817
Raegan Garlitz <raegan.garlitz@gmail.com>
2018-10-27 17:57:25 -0600

    javascript clean up

--- index.js
+++ index.js
@@ -1,73 +1,7 @@
-/*let characterName = document.querySelector('#characterName')
-let hairColor = document.querySelector('#hairColor')
-let eyeColor = document.querySelector('#eyeColor')
-let movieTitle = document.querySelector('#movieTitle')
 
-fetch('https://swapi.co/api/films', {mode: "no-cors"})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    characterName.textContent = myJson.name
-    hairColor.textContent = myJson.hair_color
-    eyeColor.textContent = myJson.eye_color
-    console.log(JSON.stringify(myJson));
-  })
-  .then(function(myJson) {
-    myJson.results.forEach(function(film) {
-      var list = document.createElement('li')
-      list.textContent = film.title
-      movieTitle.appendChild(list)
-      console.log(film.title);
-    })
-  })*/
-  
-  
   import { films } from './films.js'
   import { people } from './people.js'
   
-  /*let cardList = document.querySelector("#card-list")
-  
-  films.forEach((film) => {
-    let listItem = document.createElement('li')
-    listItem.textContent = film.title
-    cardList.appendChild(listItem)
-  })
-  
-  console.log(people[9])
-  console.log(films[5])*/
-
-  //let cardList = document.querySelector("#card-list")
-  
-  
-  
-  /*films.forEach((film) => {
-    let listItem = document.createElement('li')
-    listItem.textContent = film.title
-    cardList.appendChild(listItem)
-  })
-
-  for (var info in empire) {
-    let listItem = document.createElement('li')
-    listItem.textContent = info.director
-    cardList.appendChild(listItem)
-    console.log(info.director)
-  }*/
-
-  
-  /*let empire = films[5]
-  //console.log(movie.director)
-  //console.log(films)
- 
-  function empireStrikesBack(movie) {
-    movieCrawl.textContent = movie.title
-    //cardList.appendChild(listItem)
-  }
-  
-  
-
-  empireStrikesBack(empire)*/
-
   let movieTitle = document.querySelector("#movie-title")
   let movieDirector = document.querySelector("#movie-director")
   let releaseDate = document.querySelector("#release-date")
